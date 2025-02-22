c5513613e4a456e8e7d8a0efbb4567de73f205c9
Italo <italomatos764@gmail.com>
2025-02-22 15:12:22 -0300

    Configure Prettier and ESLint, update styles, and clean up TypeScript configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'profile', loadChildren: () => import('./features/profile/profile.module').then(m => m.ProfileModule) }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
