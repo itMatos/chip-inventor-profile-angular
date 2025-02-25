f62684a9c0dcfcb065e6d3c3038b1ab1363acc87
Italo <italomatos764@gmail.com>
2025-02-25 15:42:46 -0300

    Add FontAwesome icons to project card; implement like button with toast notifications

--- src/app/features/profile/components/project-card/project-card.component.ts
+++ src/app/features/profile/components/project-card/project-card.component.ts
@@ -1,13 +1,82 @@
 import { NgFor } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { BadgeModule, ButtonDirective, CardModule } from '@coreui/angular';
+import { Component, Input, signal } from '@angular/core';
+import {
+  BadgeModule,
+  ButtonDirective,
+  CardModule,
+  ProgressComponent,
+  ToastBodyComponent,
+  ToastComponent,
+  ToasterComponent,
+  ToastHeaderComponent,
+  TooltipDirective,
+} from '@coreui/angular';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart as regularHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as solidHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-project-card',
-  imports: [CardModule, ButtonDirective, BadgeModule, NgFor],
+  imports: [
+    CardModule,
+    ButtonDirective,
+    BadgeModule,
+    NgFor,
+    TooltipDirective,
+    FontAwesomeModule,
+    ProgressComponent,
+    ToasterComponent,
+    ToastComponent,
+    ToastHeaderComponent,
+    ToastBodyComponent,
+  ],
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss'],
 })
 export class ProjectCardComponent {
   @Input() project!: { title: string; description: string; tags: string[] };
+
+  liked = false;
+  solidHeart = solidHeart;
+  regularHeart = regularHeart;
+
+  likedProjectMessage = 'Adicionado aos favoritos';
+  unlikedProjectMessage = 'Removido dos favoritos';
+
+  position = 'bottom-center';
+  visible = signal(false);
+  percentage = signal(0);
+
+  showLikedProjectMessage() {
+    this.liked = true;
+  }
+
+  showUnlikedProjectMessage() {
+    this.liked = false;
+  }
+
+  toggleToast() {
+    if (this.liked) {
+      this.showLikedProjectMessage();
+    } else {
+      this.showUnlikedProjectMessage();
+    }
+    this.visible.update((value) => !value);
+  }
+
+  toggleLike() {
+    this.liked = !this.liked;
+
+    this.visible.set(true);
+    setTimeout(() => this.visible.set(false), 3000);
+  }
+
+  onVisibleChange($event: boolean) {
+    this.visible.set($event);
+    this.percentage.set(this.visible() ? this.percentage() : 0);
+  }
+
+  onTimerChange($event: number) {
+    this.percentage.set($event * 25);
+  }
 }
