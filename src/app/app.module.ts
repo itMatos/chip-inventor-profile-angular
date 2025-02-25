24dead203796a01152ee80718d497ddd734cde42
Italo <italomatos764@gmail.com>
2025-02-24 21:23:25 -0300

    Enhance user info component with icon buttons and updated user description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { IconModule, IconSetService } from '@coreui/icons-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileModule } from './features/profile/profile.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ProfileModule],
-  providers: [provideAnimationsAsync()],
+  imports: [BrowserModule, AppRoutingModule, ProfileModule, IconModule],
+  providers: [provideAnimationsAsync(), IconSetService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/features/profile/components/user-info/user-info.component.ts
+++ src/app/features/profile/components/user-info/user-info.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { AvatarModule, ButtonDirective, CardModule, GridModule } from '@coreui/angular';
+import { cilEnvelopeClosed, cilStar } from '@coreui/icons';
+import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
-  imports: [GridModule, CardModule, AvatarModule, ButtonDirective],
+  imports: [GridModule, CardModule, AvatarModule, ButtonDirective, IconDirective],
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss'],
 })
@@ -16,6 +18,8 @@ export class UserInfoComponent {
     photo: string;
     projects: { title: string; description: string }[];
   };
+
+  icons = { cilEnvelopeClosed, cilStar };
 
   get initials(): string {
     if (!this.user || !this.user.name) {
